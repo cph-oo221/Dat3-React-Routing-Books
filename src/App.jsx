4abd14e9e4f9e987b53c7e158b7287fd4c641bff
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-12-05 12:51:43 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import {
-  Navigate,
   Route,
   RouterProvider,
   createBrowserRouter,
--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -4,15 +4,14 @@ function MainLayout() {
   return (
     <div id="container">
       <div>
+        <div>
+          <h1 style={{ textAlign: "center" }}>React Router books</h1>
+        </div>
         <nav id="nav-bar">
           <NavLink to="/books">Books</NavLink>
           <NavLink to="/add-book">Add Book</NavLink>
           <NavLink to="/find-book">Find Book</NavLink>
         </nav>
-
-        <div>
-          <h1 style={{ textAlign: "center", color: "red" }}>React Router</h1>
-        </div>
 
         <div id="container-outlet">
           <Outlet />
