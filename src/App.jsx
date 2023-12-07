9dea0a78f9c3a1ed7a75b5ae1cfc613a2689d0af
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2023-12-07 07:59:41 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
   const bookFacade = returnVal;
 
   const routes = createBrowserRouter(
+    { basename: "/" },
     createRoutesFromElements(
       <Route path="/" element={<MainLayout />} errorElement={<NoMatch />}>
         <Route
